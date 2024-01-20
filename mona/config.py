4adf2356597eed86778220093a57e04b8655b891
ainopara <ainopara@gmail.com>
2024-01-20 09:10:13 +0800

    feat: adjust config

--- mona/config.py
+++ mona/config.py
@@ -2,26 +2,25 @@ config = {
     "height": 32,
     "train_width": 384,
     "batch_size": 128,
-    "epoch": 50,
+    "epoch": 150,
     "print_per": 100,
-    "save_per": 600,
+    "save_per": 4000,
 
-    "train_size": 200000,
-    "validate_size": 10000,
+    "train_size": 512000,
+    "validate_size": 12800,
 
     "pretrain": False,
-    "pretrain_name": "chs_all.pt",
+    "pretrain_name": "model_training.pt",
 
     # Set according to your CPU
     "dataloader_workers": 6,
     # Generate data online for train/val
-    "online_train": True,
+    "online_train": False,
     "online_val": True,
     # Freeze the cnn backbone in the first few epochs
     # set 0 to disable
-    "unfreeze_backbone_epoch": 0,
+    "unfreeze_backbone_epoch": 50,
 
     # Select model type: Genshin or StarRail
-    "model_type": "Genshin"
-    # "model_type": "WutheringWaves"
+    "model_type": "StarRail"
 }
